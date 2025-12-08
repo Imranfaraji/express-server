dd4ce317bcc59a3fd663b57e43697a7254cbdd3b
imran <imranfaraji880@gmail.com>
2025-12-08 15:47:10 +0600

    make crud api by express and postgreSql

--- src/server.ts
+++ src/server.ts
@@ -52,6 +52,70 @@ app.get('/', (req:Request, res:Response) => {
 })
 
 
+// user CRUD oparetion
+
+app.post("/users",async (req:Request,res:Response)=>{
+   const {name,email}=req.body
+
+   try{
+   const result=await pool.query(`INSERT INTO users(name,email) VALUES($1,$2) RETURNING *`,[name,email])
+    res.status(201).json({
+    success:false,
+    message: "data inserted success fully",
+    data:result.rows[0]
+   })
+   }catch(err:any){
+    res.status(500).json({
+    success:false,
+    message: err?.message
+   })
+   }
+
+
+  
+})
+
+app.get('/users', async(req:Request,res:Response)=>{
+  try{
+   const result = await pool.query(`SELECT * FROM users`)
+     res.status(201).json({
+    success:false,
+    message: "data inserted success fully",
+    data:result.rows
+   })
+
+  }catch(err:any){
+    res.status(500).json({
+    success:false,
+    message: err?.message
+   })
+  }
+})
+
+app.get('/users/:id', async (req:Request, res: Response)=>{
+    try{
+     const result = await  pool.query(`SELECT * FROM users WHERE id=$1`,[req.params.id])
+     if(result.rows.length===0){
+      res.status(404).json({
+        success:false,
+        message:'User not found'
+      })
+     }else{
+        res.status(201).json({
+    success:false,
+    message: "data inserted success fully",
+    data:result.rows
+   })
+     }
+    }catch(err:any){
+    res.status(500).json({
+    success:false,
+    message: err?.message
+   })
+   }
+})
+
+
 app.post("/",(req:Request,res:Response)=>{
   console.log(req.body)
   res.status(200).json({
